23b87b1898bdf459197c926f45b449e3306a5793
AnkurP13 <33151447+1619450@users.noreply.github.com>
2021-01-17 17:39:52 +0000

    Add project files.

---
+++ SimpleCalculator/CalcProcessor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class CalcProcessor
+    {
+        public double Calculate(string argOperator, double argFirstNumb, double argSecondNumb)
+        {
+            double result;
+            switch (argOperator.ToLower())
+            {
+                case "add":
+                case "+":
+                    result = argFirstNumb + argSecondNumb;
+                    break;
+                case "substract":
+                case "-":
+                case "minus":
+                    result = argFirstNumb - argSecondNumb;
+                    break;
+                case "times":
+                case "x":
+                case "multiply":
+                case "*":
+                    result = argFirstNumb * argSecondNumb;
+                    break;
+                case "divide":
+                case "/":
+                    result = argFirstNumb / argSecondNumb;
+                    break;
+                default:
+                    throw new InvalidOperationException("Specified operation is not recognized");
+            }
+            return result;
+        }
+    }
+}
---
+++ SimpleCalculator/InputConversion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class InputConversion
+    {
+        public double ConvertInputToNumb (string argTextInput)
+        {
+            double convertedNumb;
+            if (!double.TryParse(argTextInput, out convertedNumb)) throw new ArgumentException("Expected a Numerical value");
+            return convertedNumb;
+        }
+    }
+}
