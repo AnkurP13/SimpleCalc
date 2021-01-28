4e5d0b4094a865378f5a8f7af2c9752a5ef07d20
Ankur Patel <33151447+AnkurP13@users.noreply.github.com>
2021-01-28 15:01:22 +0000

    Added unit test

---
+++ SimpleCalculator/SimpleCalculator.Test.Unit/CalcProcessorTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SimpleCalculator.Test.Unit
+{
+    [TestClass]
+    public class CalProceesorTest
+    {
+        private readonly CalcProcessor _calcProcessor = new CalcProcessor();
+        [TestMethod]
+        public void AddToCheck() 
+        {
+            int numb1 = 2;
+            int numb2 = 3;
+            double result = _calcProcessor.Calculate("add", numb1, numb2);
+            Assert.AreEqual(5, result);
+        }
+        [TestMethod]
+        public void OperationToCheck()
+        {
+            int numb1 = 2;
+            int numb2 = 3;
+            double result = _calcProcessor.Calculate("+", numb1, numb2);
+            Assert.AreEqual(5, result);
+        }
+    }
+}
---
+++ SimpleCalculator/SimpleCalculator.Test.Unit/InputConversionTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SimpleCalculator.Test.Unit
+{
+    [TestClass]
+    public class InputConversionTest
+    {
+        private readonly InputConversion _inputConversion = new InputConversion();
+        [TestMethod]
+        public void ConvertsTheValidInput()
+        {
+            string inputnumb = "5";
+            double convertednumb = _inputConversion.ConvertInputToNumb(inputnumb);
+            Assert.AreEqual(5, convertednumb);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FailureForConverstionInputToDouble()
+        {
+            string inputnumb = "+";
+            double convertednumb = _inputConversion.ConvertInputToNumb(inputnumb);
+           
+        }
+    }
+}
